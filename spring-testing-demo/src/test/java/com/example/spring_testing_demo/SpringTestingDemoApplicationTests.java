c2a68a410103a7342d6b724b3157723d158e54ca
Dmitry Zinushin <zinushin@gmail.com>
2024-07-13 23:05:18 +0300

    add staff

--- spring-testing-demo/src/test/java/com/example/spring_testing_demo/SpringTestingDemoApplicationTests.java
+++ spring-testing-demo/src/test/java/com/example/spring_testing_demo/SpringTestingDemoApplicationTests.java
@@ -1,18 +1,20 @@
 package com.example.spring_testing_demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 import java.util.Arrays;
 
 @SpringBootTest
 class SpringTestingDemoApplicationTests {
 
-	private static Logger log = LoggerFactory.getLogger(SpringTestingDemoApplicationTests.class);
+	private static final Logger log = LoggerFactory.getLogger(SpringTestingDemoApplicationTests.class);
 
 	@Autowired
 	ApplicationContext applicationContext;
@@ -29,5 +31,10 @@ class SpringTestingDemoApplicationTests {
 
 		String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
 		log.info("beanDefinitionNames: {}", Arrays.toString(beanDefinitionNames));
+	}
+
+	@Test
+	void inject_spring_env_in_test_method(@Autowired  Environment environment) {
+		Assertions.assertNotNull(environment);
 	}
 }
